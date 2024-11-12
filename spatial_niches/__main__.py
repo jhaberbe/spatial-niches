6b4910347d6ecf109c0c2d08ba56dc3ede8d941f
jhaberbe <jhaberbe@users.noreply.github.com>
2024-11-12 09:07:32 +0000

    ✅ Ready to clone and code.

---
+++ spatial_niches/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for spatial_niches."""
+
+from spatial_niches.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from spatial_niches.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "spatial_niches"
